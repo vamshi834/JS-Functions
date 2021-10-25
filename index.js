23724c2dbc9082bd9910f6d9224e97b532050140
Vamshi Medipally <vamshi.m834@gmail.com>
2021-10-25 23:34:46 +0530

    added a new file

---
+++ index.js
@@ -0,0 +1,39 @@
+console.clear()
+
+function numsum(){
+  var num1 = 28
+  var num2 = 98
+  var sum = num1 + num2
+  return sum
+  console.log('test')
+}
+console.log(numsum())
+
+//
+function nummul(){
+  var num1 = 28
+  var num2 = 9
+  var mul = num1 * num2
+  return mul
+  console.log('test')
+}
+console.log(nummul())
+
+function numsub(){
+  var num1 = 28
+  var num2 = 98
+  var sub = num1 - num2
+  return sub
+  console.log('test')
+}
+console.log(numsub())
+
+//
+function nummodule(){
+  var num1 = 28
+  var num2 = 9
+  var module = num1 % num2
+  return module
+  console.log('test')
+}
+console.log(nummodule())
